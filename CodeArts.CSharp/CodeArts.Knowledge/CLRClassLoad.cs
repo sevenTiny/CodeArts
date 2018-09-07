ce8c91b09fea6100da0dab5ec084331cc72dbc56
wangdong3 <wangdong3@beisen.com>
2018-09-07 10:14:17 +0800

    clr class load sequnce :smirk:

---
+++ CodeArts.CSharp/CodeArts.Knowledge/CLRClassLoad.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+
+namespace CodeArts.Knowledge
+{
+    public class CLRClassLoad
+    {
+        public static CLRClassLoad Instance = new CLRClassLoad();
+
+        public void Method()
+        {
+            ClassA.Instance.Log();
+        }
+    }
+
+    public class ClassA
+    {
+        public static ClassA Instance = new ClassA();
+
+        public void Log()
+        {
+            Trace.WriteLine("ClassA->Log()");
+        }
+    }
+}
---
+++ CodeArts.CSharp/Tests/Test.Knowledge/CLRClassLoadTest.cs
@@ -0,0 +1,17 @@
+﻿using CodeArts.Knowledge;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Test.Knowledge
+{
+    public class CLRClassLoadTest
+    {
+        [Fact]
+        public void Test()
+        {
+            CLRClassLoad.Instance.Method();
+        }
+    }
+}
