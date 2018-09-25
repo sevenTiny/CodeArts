2381257b944448eaea5f4cf31cd07e2600baac2e
wangdong3 <wangdong3@beisen.com>
2018-09-25 17:16:31 +0800

    exception throw performance test :smile:

---
+++ CodeArts.CSharp/CodeArts.FrameworkKnowledge/ThrowException/ThrowExceptionPerformance.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeArts.FrameworkKnowledge.ThrowException
+{
+    public class ThrowExceptionPerformance
+    {
+        public void ThrowException()
+        {
+            int executeTimes = 10000;
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+            for (int i = 0; i < executeTimes; i++)
+            {
+                try
+                {
+                    throw new Exception("xxx");
+                }
+                catch (Exception)
+                {
+                    //do noting ...
+                }
+            }
+            stopwatch.Stop();
+            Trace.WriteLine($"{executeTimes} 次调用抛出异常耗时：{stopwatch.ElapsedMilliseconds}");
+            //超出一分钟
+        }
+    }
+}
---
+++ CodeArts.CSharp/Tests/Test.FrameworkKnowledge/ExceptionTest.cs
@@ -0,0 +1,20 @@
+﻿using CodeArts.FrameworkKnowledge.ThrowException;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test.FrameworkKnowledge
+{
+    [TestClass]
+    public class ExceptionTest
+    {
+        [TestMethod]
+        public void Test()
+        {
+            new ThrowExceptionPerformance().ThrowException();
+        }
+    }
+}
