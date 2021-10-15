e8009c6522f86df6d05b27b5243e4ac4c08b3741
wangdong3 <wangdong3@beisen.com>
2021-10-15 16:30:24 +0800

    repaire bug

--- CSharp/CodeArts.Topic/AnswerMatch.cs
+++ CSharp/CodeArts.Topic/AnswerMatch.cs
@@ -1,31 +1,31 @@
 ﻿/*
- 一个填空题最多有10个填空，每个填空最多5个答案，前提条件，开启按空给分并且不区分顺序，并且每个填空的得分相同(简化需求了)
-用户在作答完成之后，需要计算出这个填空题能得多少分。(每个填空只能得一次分，且需要按照最多正确的填空来得分)
-举例：
-
-正确答案 
-填空1： A，B
-填空2： A，C
-填空3： B
-
-用户答案：
-填空1： A（正确）
-填空2： B（正确）
-填空3： C（正确）
-
-正确答案 
-填空1： A，B
-填空2： A，C
-填空3： D
-
-用户答案：
-填空1： A（正确）
-填空2： B （正确）
-
-数学问题
-有一个一维数组a，长度为n，n小于等于10
-有一个数组集合b，长度为n，n小于等于10，集合每一项都是一个数组，长度为m，m小于等于5
-求解：一维数组中的每一项是否被数组集合中某一个数组包含，如果包含则为true，如果不包含则为false。另：数组集合中每一个数组只能使用一次，且需要保证最终为true的项最多。
+     一个填空题最多有10个填空，每个填空最多5个答案，前提条件，开启按空给分并且不区分顺序，并且每个填空的得分相同(简化需求了)
+    用户在作答完成之后，需要计算出这个填空题能得多少分。(每个填空只能得一次分，且需要按照最多正确的填空来得分)
+    举例：
+    
+    正确答案 
+    填空1： A，B
+    填空2： A，C
+    填空3： B
+    
+    用户答案：
+    填空1： A（正确）
+    填空2： B（正确）
+    填空3： C（正确）
+    
+    正确答案 
+    填空1： A，B
+    填空2： A，C
+    填空3： D
+    
+    用户答案：
+    填空1： A（正确）
+    填空2： B （正确）
+    
+    数学问题
+    有一个一维数组a，长度为n，n小于等于10
+    有一个数组集合b，长度为n，n小于等于10，集合每一项都是一个数组，长度为m，m小于等于5
+    求解：一维数组中的每一项是否被数组集合中某一个数组包含，如果包含则为true，如果不包含则为false。另：数组集合中每一个数组只能使用一次，且需要保证最终为true的项最多。
  */
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
@@ -39,17 +39,26 @@ namespace CodeArts.Topic
     public class AnswerMatch
     {
         [TestMethod]
-        [DataRow("A,B,C", "A,B|A,C|B")]
-        [DataRow("A,B,C,D", "A,B|A,C|B|D,E,F")]
-        [DataRow("A,B,C,D,E", "A,B|A,C|B|D,E,F|E")]
-        [DataRow("A,B,C,D,E", "A,B|A,C|C|D,E,F|E")]
-        [DataRow("A,B,C,D,E", "A,B|A,C|C,D|E,F|E")]
+        //[DataRow("A,B,C", "A,B|A,C|B")]
+        //[DataRow("A,B,C,D", "A,B|A,C|B|D,E,F")]
+        //[DataRow("A,B,C,D,E", "A,B|A,C|B|D,E,F|E")]
+        //[DataRow("A,B,C,D,E", "A,B|A,C|C|D,E,F|E")]
+        //[DataRow("A,B,C,D,E", "A,B|A,C|C,D|E,F|E")]
+        //[DataRow("A,B,C,D,E,F", "A,B|A,C|C,D|E,F|E|F")]
+        //[DataRow("A,B,C,D,E,F,A", "A,B|A,C|C,D|E,F|E|F|A,C,E")]
+        [DataRow("A,B,C,D,E,F,A,B", "A,B|A,C|C,D|E,F|E|F|A,C,E|D")]
+        //[DataRow("A,B,C,D,E,F,A,B,C", "A,B|A,C|C,D|E,F|E|F|A,C,E|D|C")]
+        //[DataRow("A,B,C,D,E,F,G,H,I,J", "A,B|A,C|C,D|E,F|E|F|G|H|I|O")]
         public void Main(string input, string answer)
         {
             //输入答案数组
             string[] inputs = input.Split(',');
             //标准答案
             string[][] answers = answer.Split('|').Select(t => t.Split(',')).ToArray();
+
+            Stopwatch stopwatch = new Stopwatch();
+
+            stopwatch.Start();
 
             //1. 转成1/0方形矩阵
             /*
@@ -74,15 +83,17 @@ namespace CodeArts.Topic
                 1,0,1,0 A,C
                 ...
             */
-            var max = new Tuple<int, List<int>>(0, new List<int>());
+            var max = new Store() { Sum = 0, Indexs = new int[inputs.Length], IndexsTmp = new int[inputs.Length] };
 
-            GetMax(0, rec, new bool[inputs.Length], 0, new List<int>(), ref max);
+            GetMax(0, rec, new bool[inputs.Length], 0, new List<int>(), max);
+
+            stopwatch.Stop();
 
             //输出结果
-            Trace.WriteLine($"result={string.Join(",", max.Item2.Select(t => inputs[t]))}");
+            Trace.WriteLine($"{inputs.Length} Inputs\tElapsedMilliseconds={stopwatch.ElapsedMilliseconds}\tresult={string.Join(",", max.Levels.Select(t => inputs[t]))}");
         }
 
-        private void GetMax(int level, int[][] rec, bool[] hasExist, int lineSum, List<int> selected, ref Tuple<int, List<int>> max)
+        private void GetMax(int level, int[][] rec, bool[] hasExist, int lineSum, List<int> selected, Store max)
         {
             //当前层结果
             var currentLevel = rec[level];
@@ -94,6 +105,7 @@ namespace CodeArts.Topic
             for (int i = 0; i < currentLevel.Length; i++)
             {
                 var store = new List<int>(selected);
+                var origin = lineExist[i];
 
                 if (currentLevel[i] == 1)
                 {
@@ -102,7 +114,13 @@ namespace CodeArts.Topic
                         continue;
 
                     lineExist[i] = true;
+                    max.IndexsTmp[level] = i;
                     store.Add(level);
+                }
+                else if (isLast)
+                {
+                    //最后一层不是1就不用继续计算了
+                    continue;
                 }
 
                 var sum = lineSum + currentLevel[i];
@@ -110,15 +128,34 @@ namespace CodeArts.Topic
                 //最后一层
                 if (isLast)
                 {
-                    if (sum > max.Item1)
-                        max = new Tuple<int, List<int>>(sum, store);
+                    if (sum > max.Sum)
+                    {
+                        max.Sum = sum;
+                        max.Indexs = max.IndexsTmp.Clone() as int[];
+                        max.Levels = store;
+                    }
 
                     continue;
                 }
 
-                GetMax(level + 1, rec, lineExist, sum, store, ref max);
-                lineExist[i] = false;
+                GetMax(level + 1, rec, lineExist, sum, store, max);
+
+                lineExist[i] = origin;
             }
         }
+    }
+
+    internal class Store
+    {
+        public int Sum { get; set; }
+        /// <summary>
+        /// 层级（也就是答案）
+        /// </summary>
+        public List<int> Levels { get; set; }
+        /// <summary>
+        /// 锚中的索引index
+        /// </summary>
+        public int[] Indexs { get; set; }
+        public int[] IndexsTmp { get; set; }
     }
 }
