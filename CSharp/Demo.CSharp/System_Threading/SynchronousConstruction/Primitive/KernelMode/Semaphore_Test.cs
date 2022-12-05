6aabfe847fead213c1d6a79fa6c1935dd8086728
wangdong3 <wangdong3@beisen.com>
2022-12-05 19:02:49 +0800

    rename

---
+++ CSharp/Demo.CSharp/System_Threading/SynchronousConstruction/Primitive/KernelMode/Semaphore_Test.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo.CSharp.System_Threading.SynchronousConstruction.Primitive.KernelMode
+{
+    internal class Semaphore_Test
+    {
+
+    }
+}
