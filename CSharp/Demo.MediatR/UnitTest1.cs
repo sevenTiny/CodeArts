117b31f4bb40942a928b718535dc64e8e566fb41
sevenTiny <seventiny@foxmail.com>
2023-10-27 17:09:57 +0800

    add mediatR

---
+++ CSharp/Demo.MediatR/UnitTest1.cs
@@ -0,0 +1,14 @@
+namespace Demo.MediatR
+{
+    /// <summary>
+    /// https://github.com/jbogard/MediatR
+    /// </summary>
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
